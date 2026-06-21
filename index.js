8921934c9eb448405f61301af8f6686f47ac653e
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-06-21 14:13:47 +0600

    Problem 2 solved

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+//Solution 1
 function calculateDifference(val1, val2){
     return Math.abs(val1-val2);
 }
 
 console.log(calculateDifference(1,2));
+//Solution 1 Ends
+
+//Solution 2
+function isOdd(val){
+    if(val%2==0) return false;
+    if(val%2!=0) return  true;
+}
+
+console.log(isOdd(4));
+//Solution 2 Ends
