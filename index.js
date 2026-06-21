57c4f11c199a5217751293a19776517c3e38dc09
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-06-22 01:18:43 +0600

    Problem 5 solved

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 //Solution 1
-function calculateDifference(val1, val2){
-    return Math.abs(val1-val2);
+function calculateDifference(val1, val2) {
+    return Math.abs(val1 - val2);
 }
 
-console.log(calculateDifference(1,2));
+console.log(calculateDifference(1, 2));
 //Solution 1 Ends
 
 //Solution 2
-function isOdd(val){
-    if(val%2==0) return false;
-    if(val%2!=0) return  true;
+function isOdd(val) {
+    if (val % 2 == 0) return false;
+    if (val % 2 != 0) return true;
 }
 
 console.log(isOdd(4));
 //Solution 2 Ends
 
 //Solution 3
-const nums=[];
-function findMin(nums){
+const nums = [];
+function findMin(nums) {
     console.log(Math.min(...nums));
 }
 
@@ -25,15 +25,23 @@ findMin([5, 3, 4]);
 //Solution 3 ends
 
 //Solution 4
-const numbers=[66,70, 71, 29];
-const newArr= numbers.filter(filterEvenNumbers);
-function filterEvenNumbers(number){
-    if(number%2==0){
-            return number;
+const numbers = [66, 70, 71, 29];
+const newArr = numbers.filter(filterEvenNumbers);
+function filterEvenNumbers(number) {
+    if (number % 2 == 0) {
+        return number;
     }
 }
 
 console.log(newArr);
 //Solution 4 Ends
 
+//Solution 5
+
+const old_arr = [1, 30, 4, 21, 55];
+console.log(old_arr.sort(sortArrayDescending));
+function sortArrayDescending(a,b) {
+    return b-a;
+}
+//Solution 5 Ends
 
