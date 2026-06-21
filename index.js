19f86a28c5a09eec7bcac16e08d66e5a62408976
A-N-M-Rashed <a.n.m.rashed045@gmail.com>
2026-06-22 01:57:55 +0600

    Problem 8 solved

--- index.js
+++ index.js
@@ -67,3 +67,17 @@ function findAverage(arr){
 
 findAverage([10, 20]);
 //Solution 7 Ends
+
+//Solution 8 
+function isLeapYear(year){
+    if(year%4==0 && year%100!=0 || year%400==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+console.log(isLeapYear(2004));
+//Solution 8 Ends
